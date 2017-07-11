51e94a9401f184b4df78d97ca5431cb96ff07d53
TiangeY <cangyazi@gmail.com>
2017-07-11 12:56:42 -0400

    responsive css query

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -96,7 +96,6 @@
     });
 
 
-
     $("#award-experience").click(function() {
         $("#award-background").stop().animate({
             width: "50%"
@@ -104,10 +103,179 @@
         $("#award-img1").stop().animate({
             opacity: "1"
         }, 300 );
-        $(".award-titles").stop().animate({
+        $(".award-titles").stop().hide();
+        $("#award-text-experience").stop().show({}, 100 );
+        $("#award-back-button").stop().show();
+        $("#what-awards").stop().animate({
+            top: "30%"
+        }, 300 );
+        $("#award-experience").hover(function() {
+            $("#award-background").stop().animate({
+                width: "50%"
+            }, 300 );
+            $("#award-img1").stop().animate({
+                opacity: "1"
+            }, 300 );
+        });
+    });
+
+    $("#award-team").click(function() {
+        $("#award-background").stop().animate({
+            width: "50%"
+        }, 300 );
+        $("#award-img2").stop().animate({
+            opacity: "1"
+        }, 300 );
+        $(".award-titles").stop().hide();
+        $("#award-text-team").stop().show({}, 100 );
+        $("#award-back-button").stop().show();
+        $("#what-awards").stop().animate({
+            top: "30%"
+        }, 300 );
+        $("#award-team").hover(function() {
+            $("#award-background").stop().animate({
+                width: "50%"
+            }, 300 );
+            $("#award-img2").stop().animate({
+                opacity: "1"
+            }, 300 );
+        });
+    });
+
+    $("#award-creation").click(function() {
+        $("#award-background").stop().animate({
+            width: "50%"
+        }, 300 );
+        $("#award-img3").stop().animate({
+            opacity: "1"
+        }, 300 );
+        $(".award-titles").stop().hide();
+        $("#award-text-creation").stop().show({}, 100 );
+        $("#award-back-button").stop().show();
+        $("#what-awards").stop().animate({
+            top: "30%"
+        }, 300 );
+        $("#award-creation").hover(function() {
+            $("#award-background").stop().animate({
+                width: "50%"
+            }, 300 );
+            $("#award-img3").stop().animate({
+                opacity: "1"
+            }, 300 );
+        });
+    });
+
+    $("#award-award").click(function() {
+        $("#award-background").stop().animate({
+            width: "50%"
+        }, 300 );
+        $("#award-img4").stop().animate({
+            opacity: "1"
+        }, 300 );
+        $(".award-titles").stop().hide();
+        $("#award-text-award").stop().show({}, 100 );
+        $("#award-back-button").stop().show();
+        $("#what-awards").stop().animate({
+            top: "30%"
+        }, 300 );
+        $("#award-award").hover(function() {
+            $("#award-background").stop().animate({
+                width: "50%"
+            }, 300 );
+            $("#award-img4").stop().animate({
+                opacity: "1"
+            }, 300 );
+        });
+    });
+
+
+
+
+
+    $("#award-back-button").click(function() {
+        $("#award-background").stop().animate({
+            width: "100%"
+        }, 300 );
+        $("#award-img1").stop().animate({
             opacity: "0"
         }, 300 );
+        $(".award-text").stop().hide();
+        $("#what-awards").stop().animate({
+            top: "50%"
+        }, 300 );
+        $("#award-back-button").stop().hide();
+        $(".award-titles").stop().show({}, 100 );
+        $("#award-experience").hover(function() {
+            $("#award-background").stop().animate({
+                width: "78%"
+            }, 300 );
+            $("#award-img1").stop().animate({
+                opacity: "1"
+            }, 300 );
+        }, function() {
+            $(this).parent().parent().children().each(function() {
+                $(this).stop().animate({
+                    width: $(this).data("standardWidth")
+                }, 120 );
+                $("#award-img1").stop().animate({
+                    opacity: "0"
+                }, 120 );
+            });
+        });
+        $("#award-team").hover(function() {
+            $("#award-background").stop().animate({
+                width: "78%"
+            }, 300 );
+            $("#award-img2").stop().animate({
+                opacity: "1"
+            }, 300 );
+        }, function() {
+            $(this).parent().parent().children().each(function() {
+                $(this).stop().animate({
+                    width: $(this).data("standardWidth")
+                }, 120 );
+                $("#award-img2").stop().animate({
+                    opacity: "0"
+                }, 120 );
+            });
+        });
+        $("#award-creation").hover(function() {
+            $("#award-background").stop().animate({
+                width: "78%"
+            }, 300 );
+            $("#award-img3").stop().animate({
+                opacity: "1"
+            }, 300 );
+        }, function() {
+            $(this).parent().parent().children().each(function() {
+                $(this).stop().animate({
+                    width: $(this).data("standardWidth")
+                }, 120 );
+                $("#award-img3").stop().animate({
+                    opacity: "0"
+                }, 120 );
+            });
+        });
+        $("#award-award").hover(function() {
+            $("#award-background").stop().animate({
+                width: "78%"
+            }, 300 );
+            $("#award-img4").stop().animate({
+                opacity: "1"
+            }, 300 );
+        }, function() {
+            $(this).parent().parent().children().each(function() {
+                $(this).stop().animate({
+                    width: $(this).data("standardWidth")
+                }, 120 );
+                $("#award-img4").stop().animate({
+                    opacity: "0"
+                }, 120 );
+            });
+        });
     });
+
+
 
 
 
